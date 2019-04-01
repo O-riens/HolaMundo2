0299a7caa52727a958c57f0e2e50f9922707123b
JusticeSeven <you@example.com>
2019-04-01 12:15:05 +0200

    Modificada la clase principal, de nuevo, again

--- src/holamundo2/HolaMundo2.java
+++ src/holamundo2/HolaMundo2.java
@@ -7,6 +7,7 @@ public class HolaMundo2 {
         System.out.println("H0l4 mund0");
         System.out.println("3j3mpl0 p4r4 pru3b4s");
         System.out.println("4br1l 2019");
+        System.out.println("13S LU1S V3L3Z");
 	
 
     }
