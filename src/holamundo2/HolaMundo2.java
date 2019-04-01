80a2790561c175bd4f484b192ed365e4bd7d6b6a
JusticeSeven <you@example.com>
2019-04-01 12:13:18 +0200

    Modificada la clase principal

---
+++ src/holamundo2/HolaMundo2.java
@@ -0,0 +1,14 @@
+package holamundo2;
+
+
+public class HolaMundo2 {
+   
+    public static void main(String[] args) {
+        System.out.println("H0l4 mund0");
+        System.out.println("3j3mpl0 p4r4 pru3b4s");
+        System.out.println("4br1l 2019");
+	
+
+    }
+    
+}
